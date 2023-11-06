8da8b82ffd952d9456ff6c6d4edf48617d0b2276
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-11-06 20:19:39 +0200

    Working fine now for the three

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
  
   const [ubExcuse, setubExcuse] = useState("");
   const [familyExcuse, setFamilyExcuse] = useState("");
+  const [funnyExcuse, setFunnyExcuse] = useState("");
 
   const handleGetUbExcuse = () => {
     axios.get("https://excuser-three.vercel.app/v1/excuse/unbelievable/").then((res) =>
@@ -19,18 +20,33 @@ function App() {
 
   };
   
+  const handleGetFunnyExcuse = () => {
+    axios.get("https://excuser-three.vercel.app/v1/excuse/funny/").then((res) =>
+      setFunnyExcuse(res.data[0].excuse));
+
+  };
     
   useEffect(() => {
     handleGetUbExcuse();
     handleGetFamilyExcuse();
+    handleGetFunnyExcuse(); 
   }, []); 
  
 
   return (
     <div className="App">
-      <div className="container">
-        <button onClick={handleGetUbExcuse} >Get ubExcuse</button>
+      <div className="grid-container"> <h1>Generate Excuse APP</h1> 
+       <section className='ub-grid'> <button onClick={handleGetUbExcuse} >Get ubExcuse</button>
         <p>{ ubExcuse}</p>
+        </section>
+        
+        <section className='ub-grid'> <button onClick={handleGetFamilyExcuse} >Get Family Excuse</button>
+        <p>{ familyExcuse}</p>
+        </section>
+
+        <section className='ub-grid'> <button onClick={handleGetFunnyExcuse} >Get Funny Excuse</button>
+        <p>{ funnyExcuse}</p>
+        </section>
       </div>
       </div>
   );
